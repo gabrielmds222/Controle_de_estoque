447e055f2ebba950e1f49ec3be01b6df2421e7cf
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-06-29 21:41:05 -0300

    feat: Adiciona botões de deletar e editar

--- frontend/src/components/Tabela/index.tsx
+++ frontend/src/components/Tabela/index.tsx
@@ -1,24 +1,32 @@
+import { Pencil, Trash } from "@phosphor-icons/react";
+
 const Tabela = () => {
   return (
     <tbody className="divide-y divide-gray-200">
       <tr>
         <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-          John Doe
+          1
         </td>
         <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-          24/05/1995
+          Arroz branco
         </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">
-          Web Developer
-        </td>
-        <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">4</td>
+        <td className="whitespace-nowrap px-4 py-2 text-gray-700">R$ 7,00</td>
         <td className="whitespace-nowrap px-4 py-2">
-          <a
-            href="#"
-            className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
+          <button
+            type="button"
+            className="inline-block rounded bg-green-600 p-2 text-xs font-medium text-white hover:bg-green-700"
           >
-            View
-          </a>
+            <Pencil size={24} color="#fff" weight="bold" />
+          </button>
+        </td>
+        <td className="whitespace-nowrap px-4 py-2">
+          <button
+            type="button"
+            className="inline-block rounded bg-red-600 p-2 text-xs font-medium text-white hover:bg-red-700"
+          >
+            <Trash size={24} color="#fff" weight="bold" />
+          </button>
         </td>
       </tr>
     </tbody>
