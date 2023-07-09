178cf9c49075e1e49759860bbe1ff07e3689e061
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-07-08 23:07:23 -0300

    feat: Adiciona métodos do produto

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
-
+Route::post('/login', [UserController::class, 'login']);
 Route::post('/users', [UserController::class, 'store']);
+
+Route::get('/product', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
 
 Route::get('/', function(){
     return response()->json([
-        'hello ' => true
+        'success' => true
     ]);
 });
