ce1369cffbb656ae230c217424d9c3820101ec0e
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-07-10 02:16:40 -0300

    feat: Funções de edição e deletar no controller de produtos

--- backend/app/Http/Controllers/API/ProductController.php
+++ backend/app/Http/Controllers/API/ProductController.php
@@ -26,4 +26,25 @@ class ProductController extends Controller
         return new ProductResource($product);
     }
 
+    public function destroy($id)
+    {
+        $product = Produto::findOrFail($id);
+        $product->delete();
+        return response()->json(['message' => 'Produto excluído com sucesso']);
+    }
+
+    public function update(Request $request, $id) {
+        $product = Produto::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    
+        $data = $request->all();
+    
+        $product->update($data);
+    
+        return new ProductResource($product);
+    }
+
 }
